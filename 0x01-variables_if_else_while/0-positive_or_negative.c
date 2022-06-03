a1900f5db24db9d4dce2e3a0f24a815c7af2a26c
merraa <meron12345@yahoo.com>
2022-06-03 10:58:12 -0700

    0th task 4th attempt

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,17 +1,25 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 
 int main(void)
 {
-	int n; /*declaring a local variable*/
+	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0) {
+	if (n > 0) 
+	{
 		printf("%d is positive\n", n);
-	} else if (n < 0) {
+	} else if (n < 0) 
+	{
 		printf("%d is negative\n", n);
-	} else {
+	} else 
+	{
 		printf("%d is zero\n", n);
 	}
 	return (0);
