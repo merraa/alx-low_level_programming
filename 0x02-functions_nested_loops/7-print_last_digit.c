cfd5285ff6461bf2677a7880de321df29e1e3635
merraa <meron12345@yahoo.com>
2022-06-08 04:01:42 -0700

    i hate tele

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: number whose last digit is bieng determined
+ *
+ * Return: Always (0)
+ */
+
+int print_last_digit(int n)
+{
+	n = n % 10;
+	
+	if (n < 0)
+		n = -n;
+		_putchar(n + '0');
+		return (n);
+}
