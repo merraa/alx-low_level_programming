abf4cac69efa2605a954e89ec54829900188f93a
merraa <meron12345@yahoo.com>
2022-06-08 03:42:35 -0700

    i hate telee

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * int _abs - computes absolute value of an integer
+ *
+ * Return: Always 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+	return (0);
+}
