29de2e1c415a6500463faa4958e45d4a09ccb23b
merraa <meron12345@yahoo.com>
2022-06-09 20:51:26 -0700

    j

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -8,17 +8,8 @@
 
 int _isdigit(int c)
 {
-	char i;
-	int isdigit = 0;
-
-	for (i = 0; i <= 9; i++)
-	{
-		if (i == c)
-		{
-			isdigit = 1;
-			break;
-		}
-	}
-
-	return (isdigit);
+	if(c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
 }
