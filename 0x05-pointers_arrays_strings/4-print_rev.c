6d8a0acab2a9ff82f533944d4953867152e2edc6
merraa <meron12345@yahoo.com>
2022-06-13 10:09:00 -0700

    fixed a lot

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,12 +10,15 @@
 void print_rev(char *s)
 {
 	int i;
+	int c = 0;
 
-	while (s[i] != '\0')
-		i++;
-	
-	while (i)
-		_putchar(s[--i]);
-	
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		c++;
+	}
+	for (i = c - 1; i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
 	_putchar('\n');
 }
