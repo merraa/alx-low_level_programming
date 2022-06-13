68d05917a654581950490c0d5f928c1322af4b4b
merraa <meron12345@yahoo.com>
2022-06-13 07:12:52 -0700

    fixed betty style coding error again

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - a function that takes a pointer to an int as parameter and 
+ * reset_to_98 - a function that takes a pointer to an int as parameter and
  * updates the value it points to to 98.
  * @n: pointer to be used
  * Return: void
