488aaeb0417745f8d3019eeda055d00a2c0213e3
merraa <meron12345@yahoo.com>
2022-06-13 11:21:02 -0700

    6th task first attempt

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one char out of 2 of a string
+ * @str: char being printed
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		if ((a % 2) == 0)
+			_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
