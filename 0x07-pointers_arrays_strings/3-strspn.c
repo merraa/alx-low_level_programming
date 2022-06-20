a20d269cd2c4097a990cfda654113893b2509f84
merraa <meron12345@yahoo.com>
2022-06-20 12:08:48 -0700

    kl

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - Gets the length of a prefix substring
+ * @s: String that is tested
+ * @accept: bytes checked
+ * Return: Number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0, j, a = 1;
+	unsigned int count = 0;
+
+	while (s[i] != '\0' && a == 1)
+	{
+		a = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+				a = 1;
+				break;
+			}
+		}
+		i++;
+	}
+	return (count);
+}
