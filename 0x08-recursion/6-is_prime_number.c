4e8f81c3dc95f20e1e2bae06c90fa0b85abc3cf5
merraa <meron12345@yahoo.com>
2022-06-22 10:27:45 -0700

    lk

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * prime - checks whether a number is prime or not prime
+ * @x: number being tested
+ * @y: number being tested
+ * Return: the function prime
+ */
+
+int prime(int x, int y)
+{
+	if (x < 2)
+		return (0);
+	if (x / 2 < y)
+		return (1);
+	if (x % y == 0)
+		return (0);
+	return (prime(x, y + 1));
+}
+
+/**
+ * is_prime_number - returns 1 if the input integer is a
+ * prime number
+ * @n: number being tested
+ * Return: Always 0.
+ */
+
+int is_prime_number(int n)
+{
+	return (prime(n, 2));
+}
