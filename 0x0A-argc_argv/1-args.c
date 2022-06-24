641e2f2ecd0fcec0b8572ca30b0d7c31774dbcb3
merraa <meron12345@yahoo.com>
2022-06-24 12:16:15 -0700

    lo

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,12 +2,12 @@
 
 /**
  * main - program starts here
- * @argc - number of arguments passed
- * @argv - string of arguments passed
+ *@argc - number of arguments passed
+ *@argv - string of arguments passed
  * Return: Always (0)
  */
 
-int main(int argc, char* argv[]__attribute__((unused)))
+int main(int argc, char *argv[]__attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
