d5f32725242317d0292041c9ccd042ab0458304b
merraa <meron12345@yahoo.com>
2022-06-24 12:58:15 -0700

    lp

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Prints th programs name, followed by a newline
- * @argc: count of arguments
+ *@argc: count of arguments
  *@argv: array of pointers to the strings passed
  * Return: Always 0.
  */
 
-int main(int argc __attribute__((unused)), char* argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
