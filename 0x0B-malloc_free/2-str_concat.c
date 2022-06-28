c6131b01805d583ce65d0e18184b9875b3eb6772
merraa <meron12345@yahoo.com>
2022-06-28 10:38:56 -0700

    jk

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,12 +24,12 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	if (s1 == NULL)
 		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		*(m + i) = *(s1 + i);
 	}
+	if (s2 == NULL)
+		s2 = "";
 	for (j = 0; s2[j] != '\0'; j++)
 	{
 		*(m + i + j) = *(s2 + j);
