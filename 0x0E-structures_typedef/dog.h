3235c118c3c3efc2cb19c2e1fe91907c93e8c97c
merraa <meron12345@yahoo.com>
2022-07-04 12:36:50 -0700

    kl

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - defines a new type struct dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ * Description: This is a structure which defines a new type dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
