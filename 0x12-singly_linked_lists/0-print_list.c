3c35cd0bdea186e7a0821dafec9378341d24a0a7
merraa <meron12345@yahoo.com>
2022-07-14 08:01:53 -0700

    lkl

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - Prints all the elements 
+ * f a list_t list
+ * @h: Pointer to elements of the list
+ * Return: 0 if str is null
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count;
+
+	if (h == NULL)
+		return (0);
+	for (count = 0; h != NULL; count++)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (count);
+}
