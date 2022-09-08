daabf67fb34002b20ff5d41c78c865942719db5d
merraa <104758118+merraa@users.noreply.github.com>
2022-09-08 21:21:51 +0300

    Delete 0x18-dynamic_libraries directory

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -P /tmp https://raw.githubusercontent.com/samuelsrmv/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/library.so
-export LD_PRELOAD=/tmp/library.so
